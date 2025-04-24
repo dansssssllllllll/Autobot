a40086c27fc9e85e31e3fb9e96a2e54209e1dcde
dansssssllllllll <gujogujo18@gmail.com>
2025-04-24 15:48:29 +0800

    Update index.js

--- index.js
+++ index.js
@@ -4,25 +4,28 @@ const path = require('path');
 const SCRIPT_FILE = "auto.js";
 const SCRIPT_PATH = path.join(__dirname, SCRIPT_FILE);
 
+let restartCount = 0;
+const maxRestarts = 10;
 
 function start() {
-    const main = spawn("node", [SCRIPT_PATH], {
-        cwd: __dirname,
-        stdio: "inherit",
-        shell: true
-    });
+  const main = spawn("node", [SCRIPT_PATH], {
+    cwd: __dirname,
+    stdio: "inherit",
+    shell: true
+  });
 
-    main.on("close", (exitCode) => {
-        if (exitCode === 0) {
-            console.log("Main process exited with code 0");
-        } else if (exitCode === 1) {
-            console.log("Main process exited with code 1. Restarting...");
-            start();
-        }  else {
-            console.error(`Main process exited with code ${exitCode}`);
-        }
-    });
+  main.on("close", (exitCode) => {
+    if (exitCode === 0) {
+      console.log("Main process exited with code 0");
+    } else if (restartCount < maxRestarts) {
+      restartCount++;
+      console.log(`Main process exited with code ${exitCode}. Restarting... (Attempt ${restartCount}/${maxRestarts})`);
+      start();
+    } else {
+      console.error(`Maximum restarts reached. Exiting.`);
+      process.exit(1);
+    }
+  });
 }
 
 start();
-
